f5171724a3cfe03aa6fa5757e5841e9e85b0f6bd
Jacob Janz <jacobjanzcs@gmail.com>
2020-02-01 16:52:10 -0500

    Implement getFullLocation()

--- src/main/java/com/JanzEvie/supercoolweatherapp/GeoCoords.java
+++ src/main/java/com/JanzEvie/supercoolweatherapp/GeoCoords.java
@@ -8,6 +8,7 @@ import kong.unirest.json.JSONObject;
 public class GeoCoords {
     public Float lat;
     public Float lng;
+    private static final String baseUrl = "https://maps.googleapis.com/maps/api/geocode/json"
 
     public GeoCoords(float lat, float lng) {
         this.lat = lat;
@@ -17,10 +18,7 @@ public class GeoCoords {
     // 1600+Amphitheatre+Parkway,+Mountain+View,+CA
     // and uses Google's Geocoding API to turn it into a lat/long
     public GeoCoords(String address) {
-        HttpResponse<JsonNode> response = Unirest.get("https://maps.googleapis.com/maps/api/geocode/json").
-                queryString("key", ApiKey.key).
-                queryString("address", address).asJson();
-        JSONObject responseJson = response.getBody().getObject();
+        JSONObject responseJson = getGeocodeJson(address);
         if (responseJson.getJSONArray("results").length() == 0) {
             throw new RuntimeException("Address not found or invalid.");
         }
@@ -28,6 +26,18 @@ public class GeoCoords {
                 getJSONObject("geometry").getJSONObject("location");
         lat = coordsJson.getFloat("lat");
         lng = coordsJson.getFloat("lng");
+    }
+
+    public static String getFullLocation(String address) {
+        JSONObject responseJson = getGeocodeJson(address);
+        return responseJson.getJSONArray("results").getJSONObject(0).getString("formatted_address");
+    }
+
+    private static JSONObject getGeoCodeJson(String address) {
+        HttpResponse<JsonNode> response = Unirest.get(baseUrl).
+                queryString("key", ApiKey.key).
+                queryString("address", address).asJson();
+        return response.getBody().getObject();
     }
 
     @Override
