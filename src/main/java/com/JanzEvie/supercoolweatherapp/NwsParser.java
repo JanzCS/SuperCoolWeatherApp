90225a695e2cf57f624a4893c3cd6f0a04a5fd25
Jacob Janz <jacobjanzcs@gmail.com>
2020-02-01 14:04:12 -0500

    Refactor HTTP connection to getForecastJsonArray, implement getCurrentWeather

--- src/main/java/com/JanzEvie/supercoolweatherapp/NwsParser.java
+++ src/main/java/com/JanzEvie/supercoolweatherapp/NwsParser.java
@@ -10,24 +10,36 @@ import java.time.OffsetDateTime;
 import java.util.Date;
 
 public class NwsParser {
+    private static final String baseUrl = "https://api.weather.gov/points/";
+
     public static void main(String[] args) {
-        Forecast[] test = getSevenDayForecast(args[0]);
-        System.out.println(test[0]);
+        Forecast test = getCurrentWeather(args[0]);
+        System.out.println(test);
     }
-    public static Forecast[] getSevenDayForecast(String arg) {
-        GeoCoords test = new GeoCoords(arg);
-        HttpResponse<JsonNode> response = Unirest.get("https://api.weather.gov/points/" + test.toString()).asJson();
-        if (response.getStatus() != 200) {
-            throw new RuntimeException("HTTP Code " + response.getStatus() + ": " + response.getStatusText());
-        }
-        response = Unirest.get(response.getBody().getObject().getJSONObject("properties").getString("forecast")).asJson();
-        JSONObject responseJson = response.getBody().getObject();
-        JSONArray forecastsJson = responseJson.getJSONObject("properties").getJSONArray("periods");
+
+    public static Forecast[] getSevenDayForecast(String address) {
+        JSONArray forecastsJson = getForecastJsonArray(address, "forecast");
         Forecast[] forecasts = new Forecast[forecastsJson.length()];
         for (int i = 0; i < forecastsJson.length(); i++) {
             Forecast forecast = new Forecast(forecastsJson.getJSONObject(i));
             forecasts[i] = forecast;
         }
         return forecasts;
+    }
+
+    public static Forecast getCurrentWeather(String address) {
+        JSONArray forecastsJson = getForecastJsonArray(address, "forecastHourly");
+        return new Forecast(forecastsJson.getJSONObject(0));
+    }
+
+    private static JSONArray getForecastJsonArray(String address, String forecastType) {
+        GeoCoords coords = new GeoCoords(address);
+        HttpResponse<JsonNode> response = Unirest.get(baseUrl + coords.toString()).asJson();
+        if (response.getStatus() != 200) {
+            throw new RuntimeException("HTTP Code " + response.getStatus() + ": " + response.getStatusText());
+        }
+        response = Unirest.get(response.getBody().getObject().getJSONObject("properties").getString(forecastType)).asJson();
+        JSONObject responseJson = response.getBody().getObject();
+        return responseJson.getJSONObject("properties").getJSONArray("periods");
     }
 }
