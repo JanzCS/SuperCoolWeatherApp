90353acbf347cffb9cae45ec668473fe6ea3b549
Jacob Janz <jacobjanzcs@gmail.com>
2020-02-01 18:37:07 -0500

    Add image reader to UserInterface.java

--- src/main/java/com/JanzEvie/supercoolweatherapp/UserInterface.java
+++ src/main/java/com/JanzEvie/supercoolweatherapp/UserInterface.java
@@ -1,9 +1,13 @@
 package com.JanzEvie.supercoolweatherapp;//package com.JanzEvie.app;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class UserInterface extends JPanel
@@ -162,10 +166,17 @@ public class UserInterface extends JPanel
      *********************************************************/
     public void paintForecastToday( Graphics g )
     {
-       g.drawString( forecast[ 0 ].toString(), 200,150 );
-       g.drawString( forecast[ 0 ].detailedForecast, 100,200 );
-       g.drawString( forecast[ 1 ].toString(), 200,250 );
-       g.drawString( forecast[ 1 ].detailedForecast, 100,300 );
+        try {
+            g.drawImage(ImageIO.read(new URL(forecast[0].icon)),100,100, null);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        g.drawString( forecast[ 0 ].toString(), 200,150 );
+        g.drawString( forecast[ 0 ].detailedForecast, 100,200 );
+        g.drawString( forecast[ 1 ].toString(), 200,250 );
+        g.drawString( forecast[ 1 ].detailedForecast, 100,300 );
 
     }//paintForecastToday
 
@@ -178,7 +189,6 @@ public class UserInterface extends JPanel
             g.drawString( forecast[ i ].toString(), 200,( 20 + 25 * ( i + 1 )));
             //g.drawImage( forecast[ i ].icon, 0, 0, this); // see javadoc for more info on the parameters
         }
-
     }//paintSevenDayForecast
 
     /*********************************************************
